3d8052bb882acd9580cb9e68fcad4420406c4357
Panagiotis Argyropoulos <pargyropoulos@gmail.com>
2022-08-27 17:58:24 +0300

    Add files via upload

---
+++ Code/Bcd2Hex.asm
@@ -0,0 +1,36 @@
+#include "P16F648A.inc"  ;include the defaults for the chip
+errorlevel 2
+GLOBAL Bcd2Hex
+
+Temp_Variables UDATA_OVR
+Var1 res 1
+Var2 res 1
+
+PROG1 Code
+
+; ------------------------------------ 
+; Binary to Hex Conversion
+; W should hold the data to convert
+; On exit W holds the converted Data
+; ------------------------------------ 
+
+Bcd2Hex
+	movwf Var1		;Var1 = BCD eg bcd'49'
+	swapf Var1,W	;Var1= '94'
+	andlw 0x0f
+	movwf Var2
+	bcf STATUS,C
+	rlf Var2
+	rlf Var2
+	rlf Var2
+	addwf Var2,F
+	addwf Var2,F
+
+	bcf STATUS,C
+	movf Var1,W
+	andlw 0x0f
+	addwf Var2,W
+return
+
+
+end
---
+++ Code/Hex2Dec.asm
@@ -0,0 +1,57 @@
+include "P16F648A.inc"  ;include the defaults for the chip
+errorlevel 2
+GLOBAL Hex2Dec
+Global Tens
+Global Ones
+Global BCD
+
+Temp_Variables UDATA_OVR
+Var1 res 1
+;Var2 res 1
+
+Bank0 UDATA
+Tens res 1
+Ones res 1
+BCD res 1
+
+
+PROG1 Code
+
+Hex2Dec
+	clrf Tens
+	clrf Ones
+	movwf Var1
+	movlw 0x0A
+Loop
+	subwf Var1
+	incf Tens
+	btfss STATUS,C
+	goto $+2
+goto Loop
+
+	decf Tens
+	addwf Var1
+	movf Var1,W
+	movwf Ones
+
+	movwf Var1
+	movf Tens,W
+	;movwf Var2
+	movwf BCD
+
+	movlw 0xf0
+	xorwf Var1,F
+	;swapf Var2,F
+	swapf BCD,F
+	
+	movlw 0x0f
+	;xorwf Var2,W
+	xorwf BCD,W
+	andwf Var1,W
+	movwf BCD
+
+
+return
+
+
+end
